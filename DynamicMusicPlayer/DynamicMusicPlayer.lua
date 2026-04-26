3311820da771506713268a1830056375ec9a65d8
Damgam <c_2_damiano@op.pl>
2026-04-26 14:28:48 +0200

    Oopsie

--- DynamicMusicPlayer/DynamicMusicPlayer.lua
+++ DynamicMusicPlayer/DynamicMusicPlayer.lua
@@ -1,6 +1,7 @@
 ---@ext
 CSPBuild = ac.getPatchVersionCode()
 math.randomseed(os.preciseClock())
+globalDT = 0.16
 
 CoverArtExportCanvas = ui.ExtraCanvas(vec2(256, 256), 1, render.AntialiasingMode.ExtraSharpCMAA)
 function RenderCoverArtExportCanvas()
@@ -131,12 +132,9 @@ ConfigMinimumProximityVolume = ConfigFile:get("settings", "minproximityvolume", 
 ConfigMinimumSpeedVolume = ConfigFile:get("settings", "minspeedvolume", 0.5)
 ConfigMinimumPauseVolume = ConfigFile:get("settings", "minpausevolume", 0.1)
 
-EnableNowPlayingIcon = ConfigFile:get("settings", "nowplayingicon", true)
-EnableAnimatedNowPlayingIcon = ConfigFile:get("settings", "nowplayinganimatedicon", false)
-EnableNowPlayingTime = ConfigFile:get("settings", "nowplayingtime", true)
+NowPlayingWidgetDir = ConfigFile:get("settings", "nowplayingwidgetdir", "Default")
 NowPlayingWidgetSize = ConfigFile:get("settings", "nowplayingscale", 1)
-EnableNowPlayingWidgetFadeout = ConfigFile:get("settings", "nowplayingfadeout", true)
-NowPlayingWidgetFadeoutTime = ConfigFile:get("settings", "nowplayingfadeouttime", 10)
+EnableOBSExport = ConfigFile:get("settings", "enableobsexport", false)
 
 MusicListHideWithCoverArt = ConfigFile:get("settings", "musiclisthidewithcoverart", false)
 
@@ -277,20 +275,6 @@ PlayerBestLapTime           = 180
 AverageSpeed                = 100
 TopSpeed                    = 0
 CautionVolume               = 1
-
-NowPlayingBars = {
-    [1] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [2] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [3] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [4] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [5] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [6] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-    [7] = {current = math.random(0,128)/128, target = math.random(0,128)/128},
-}
-
-NowPlayingOpacityCurrent = 0
-NowPlayingOpacityTarget = 0
-
 
 function updateConfig()
     local MasterVolume = ac.getAudioVolume('main')
@@ -495,12 +479,14 @@ function updateRaceStatusData()
         FadeOutSpeedMultiplier = 1
     end
 
-    if CurrentTrack then
-        local nowplayingOBStext = "Now Playing: " .. "(" .. string_formatTime(math.ceil(CurrentTrack:currentTime())) .. "/" .. string_formatTime(math.ceil(CurrentTrack:duration())) .. ") "
-        io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/nowplayingtime.txt", nowplayingOBStext, false)
-    else
-        local nowplayingOBStext = ""
-        io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/nowplayingtime.txt", nowplayingOBStext, false)
+    if EnableOBSExport then
+        if CurrentTrack then
+            local nowplayingOBStext = "Now Playing: " .. "(" .. string_formatTime(math.ceil(CurrentTrack:currentTime())) .. "/" .. string_formatTime(math.ceil(CurrentTrack:duration())) .. ") "
+            io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/nowplayingtime.txt", nowplayingOBStext, false)
+        else
+            local nowplayingOBStext = ""
+            io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/nowplayingtime.txt", nowplayingOBStext, false)
+        end
     end
 end
 updateRaceStatusData()
@@ -1126,9 +1112,11 @@ function getNewTrack()
             trackMemoryFile:set("lastplayedtimes", "time", os.time())
             trackMemoryFile:save()
 
-            io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/tracktitle.txt", CurrentlyPlaying, false)
-            CoverArtExportCanvas:update(RenderCoverArtExportCanvas)
-            CoverArtExportCanvas:save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/CoverArt.png")
+            if EnableOBSExport then
+                io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/tracktitle.txt", CurrentlyPlaying, false)
+                CoverArtExportCanvas:update(RenderCoverArtExportCanvas)
+                CoverArtExportCanvas:save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/CoverArt.png")
+            end
 
             break
         end
@@ -1140,6 +1128,7 @@ end
 UpdateCounter = 0
 SkipAttempts = 0
 function script.update(dt)
+    globalDT = dt
 
     --ac.debug("sunangle", ac.getSunAngle())
     local gameDt = ac.getGameDeltaT()
@@ -1301,9 +1290,11 @@ function PlaySelectedTrack(selectedTrack)
     DontSkipCurrentTrack = true
     NowPlayingOpacityCurrent = 0
     nowplayingiconcoverart = getCoverArt(CurrentlyPlaying)
-    io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/tracktitle.txt", CurrentlyPlaying, false)
-    CoverArtExportCanvas:update(RenderCoverArtExportCanvas)
-    CoverArtExportCanvas:save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/CoverArt.png")
+    if EnableOBSExport then
+        io.save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/tracktitle.txt", CurrentlyPlaying, false)
+        CoverArtExportCanvas:update(RenderCoverArtExportCanvas)
+        CoverArtExportCanvas:save(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/OBS Stuff/CoverArt.png")
+    end
 end
 
 function HandleMusicListItem(item)
@@ -1715,7 +1706,21 @@ function BehaviourTab()
     end
 end
 
+WidgetsDir = '/NowPlaying Widgets'
+WidgetsList = table.map(io.scanDir( __dirname .. WidgetsDir, '*'), function (x) return { string.sub(x, 1, #x - 4), WidgetsDir .. '/' .. x } end)
+table.sort(WidgetsList, function(a,b) return string.upper(a[1]) < string.upper(b[1]) end)
+
 function NowPlayingWidgetTab()
+    checkbox = ui.checkbox("Enable OBS Export (Can cause stutters on track switch)", EnableOBSExport)
+    if ui.itemHovered() then
+        ui.setTooltip('Exports track title and cover art to OBS Stuff folder, so you can use it for displaying outside of the game, not necesarily in OBS')
+    end
+    if checkbox then
+        EnableOBSExport = not EnableOBSExport
+        ConfigFile:setAndSave("settings", "enableobsexport", EnableOBSExport)
+    end
+    ui.separator()
+    ui.separator()
 
     ui.text('Widget size')
     local sliderValue6 = ConfigFile:get("settings", "nowplayingscale", 1)
@@ -1725,47 +1730,41 @@ function NowPlayingWidgetTab()
         ConfigFile:set("settings", "nowplayingscale", sliderValue6)
         NeedToSaveConfig = true
     end
-
-    checkbox = ui.checkbox("Enable Icon (Or Cover Art if available)", EnableNowPlayingIcon)
-    if checkbox then
-        EnableNowPlayingIcon = not EnableNowPlayingIcon
-        ConfigFile:set("settings", "nowplayingicon", EnableNowPlayingIcon)
-        NeedToSaveConfig = true
-    end
     
-    if EnableNowPlayingIcon then
-        checkbox = ui.checkbox("Enable Animated Icon (Disables Cover Arts)", EnableAnimatedNowPlayingIcon)
-        if checkbox then
-            EnableAnimatedNowPlayingIcon = not EnableAnimatedNowPlayingIcon
-            ConfigFile:set("settings", "nowplayinganimatedicon", EnableAnimatedNowPlayingIcon)
-            NeedToSaveConfig = true
+    ui.separator()
+
+    ui.text('Widget Selection:')
+    ui.separator()
+    ui.separator()
+    for i = 1, #WidgetsList do
+        if io.fileExists(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer" .. WidgetsList[i][2] .. "/Widget.lua") then
+            ui.button(string.sub(WidgetsList[i][2], 21))
+            if ui.itemHovered() then
+                ui.setTooltip('Switch (Restarts the music player)')
+            end
+            if ui.itemClicked(ui.MouseButton.Left, false) then
+                NowPlayingWidgetDir = string.sub(WidgetsList[i][2], 21)
+                ConfigFile:setAndSave("settings", "nowplayingwidgetdir", string.sub(WidgetsList[i][2], 21))
+                ac.restartApp()
+                ac.setClipboadText(string.sub(WidgetsList[i][2], 21))
+            end
+            if string.sub(WidgetsList[i][2], 21) == NowPlayingWidgetDir then
+                ui.sameLine()
+                ui.dwriteText("⬅")
+            end
+        else
+            ui.text(string.sub(WidgetsList[i][2], 21) .. " -")
+            ui.sameLine()
+            ui.text("MISSING Widget.lua")
         end
     end
+    ui.separator()
+    ui.separator()
+    ui.text("Options: ")
+    ui.separator()
+    ui.separator()
 
-    checkbox = ui.checkbox("Enable Timer", EnableNowPlayingTime)
-    if checkbox then
-        EnableNowPlayingTime = not EnableNowPlayingTime
-        ConfigFile:set("settings", "nowplayingtime", EnableNowPlayingTime)
-        NeedToSaveConfig = true
-    end
-
-    checkbox = ui.checkbox("Show the widget only when new track starts", EnableNowPlayingWidgetFadeout)
-    if checkbox then
-        EnableNowPlayingWidgetFadeout = not EnableNowPlayingWidgetFadeout
-        ConfigFile:set("settings", "nowplayingfadeout", EnableNowPlayingWidgetFadeout)
-        NeedToSaveConfig = true
-    end
-
-    if EnableNowPlayingWidgetFadeout then
-        ui.text('Show the widget for first ' .. math.ceil(NowPlayingWidgetFadeoutTime) .. ' seconds of new track')
-        local sliderValue7 = ConfigFile:get("settings", "nowplayingfadeouttime", 10)
-        sliderValue7 = ui.slider("Seconds (Default 10) ##slider7", sliderValue7, 5, 30, "%.1f")
-        if NowPlayingWidgetFadeoutTime ~= sliderValue7 then
-            NowPlayingWidgetFadeoutTime = sliderValue7
-            ConfigFile:set("settings", "nowplayingfadeouttime", sliderValue7)
-            NeedToSaveConfig = true
-        end
-    end
+    NowPlayingOptions()
 end
 
 function KeybindsTab()
@@ -1825,108 +1824,11 @@ function script.windowMain()
     end
 end
 
-ac.setWindowSizeConstraints('main', vec2(650,100), vec2(650,600))
-local nowplayingicon = ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/" .. "iconFlipped.png"
-local nowplayingbar = ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/" .. "nowplayingbar.png"
--- Now Playing
-function script.windowNowPlaying()
-    ui.columns(2, false, "NowPlayingColumns")
-    ui.beginOutline()
-    local windowWidthRange = 75
-    if EnableNowPlayingIcon and NowPlayingOpacityCurrent > 0 then
-        if EnableAnimatedNowPlayingIcon then
-            if UpdateCounter%20 == 0 or RefreshBarTargets then
-                NowPlayingBars = {
-                    [1] = {current = NowPlayingBars[1].current, target = math.random(0,128)/128},
-                    [2] = {current = NowPlayingBars[2].current, target = math.random(0,128)/128},
-                    [3] = {current = NowPlayingBars[3].current, target = math.random(0,128)/128},
-                    [4] = {current = NowPlayingBars[4].current, target = math.random(0,128)/128},
-                    [5] = {current = NowPlayingBars[5].current, target = math.random(0,128)/128},
-                    [6] = {current = NowPlayingBars[6].current, target = math.random(0,128)/128},
-                    [7] = {current = NowPlayingBars[7].current, target = math.random(0,128)/128},
-                }
-            end
-
-            RefreshBarTargets = true
-            for barIndex, barValue in ipairs(NowPlayingBars) do
-                if barValue.current > barValue.target then
-                    NowPlayingBars[barIndex].current = barValue.current - ((barValue.current - barValue.target)*0.05)
-                    RefreshBarTargets = false
-                elseif barValue.current < barValue.target then
-                    NowPlayingBars[barIndex].current = barValue.current - ((barValue.current - barValue.target)*0.05)
-                    RefreshBarTargets = false
-                end
-                if NowPlayingWidgetSize < 0.80 then
-                    barIndex = barIndex+1
-                end
-                if not (NowPlayingWidgetSize < 0.80 and barIndex == 1) then
-                    ui.drawImage(nowplayingbar, vec2((((barIndex)*12)+2)*NowPlayingWidgetSize, (10+50*NowPlayingWidgetSize)*(1-(barValue.current))), vec2((((barIndex+1)*12)+2)*NowPlayingWidgetSize,15+65*NowPlayingWidgetSize), rgbm(1, 1, 1, NowPlayingOpacityCurrent), vec2(1,1), vec2(0,0))
-                end
-            end
-            windowWidthRange = 14*7
-        else
-            if nowplayingiconcoverart then
-                ui.drawImage(nowplayingiconcoverart, vec2(10+10*NowPlayingWidgetSize,10+10*NowPlayingWidgetSize), vec2(10+70*NowPlayingWidgetSize,15+65*NowPlayingWidgetSize), rgbm(1, 1, 1, NowPlayingOpacityCurrent), vec2(0,0), vec2(1,1), ui.ImageFit.Fit)
-            else
-                ui.drawImage(nowplayingicon, vec2(10+10*NowPlayingWidgetSize,10+10*NowPlayingWidgetSize), vec2(10+70*NowPlayingWidgetSize,15+65*NowPlayingWidgetSize), rgbm(1, 1, 1, NowPlayingOpacityCurrent), vec2(1,1), vec2(0,0), ui.ImageFit.Fit)
-            end
-            
-        end
-    end
-    ui.nextColumn()
-    ui.setColumnWidth(0, windowWidthRange*NowPlayingWidgetSize)
-    ui.setColumnWidth(1, 10000)
-    ui.beginOutline()
-    ui.pushDWriteFont('OPTIEdgarBold:\\Fonts;Weight=Medium')
-    ui.dwriteText("", math.floor(5*NowPlayingWidgetSize), rgbm(1, 1, 1, NowPlayingOpacityCurrent))
-    
-    local text1
-    local text2
-    if not EnableNowPlayingTime then
-        text1 = "Now Playing: "
-    elseif EnableNowPlayingTime and CurrentTrack then
-        text1 = "Now Playing: " .. "(" .. string_formatTime(math.ceil(CurrentTrack:currentTime())) .. "/" .. string_formatTime(math.ceil(CurrentTrack:duration())) .. ") "
-    end
-    text2 = CurrentlyPlaying or ""
-    local windowWidth = math.max(ui.measureDWriteText(text1, 25*NowPlayingWidgetSize, -1).x, ui.measureDWriteText(text2, 25*NowPlayingWidgetSize, -1).x)
-    local windowWidth = vec2(windowWidth)
-    ui.dwriteText(text1, 18*NowPlayingWidgetSize, rgbm(1, 1, 1, NowPlayingOpacityCurrent))
-    ui.dwriteText(text2, 21*NowPlayingWidgetSize, rgbm(1, 1, 1, NowPlayingOpacityCurrent))
-    ui.endOutline(0, 1.5)
-    ui.popDWriteFont()
-
-    
-    ac.setWindowSizeConstraints('nowplaying', vec2(windowWidth.x+windowWidthRange+75*NowPlayingWidgetSize,100*NowPlayingWidgetSize), vec2(windowWidth.x+windowWidthRange+75*NowPlayingWidgetSize,100*NowPlayingWidgetSize))
-
-    if CurrentTrack and ((not EnableNowPlayingWidgetFadeout) or (CurrentTrack:currentTime() < NowPlayingWidgetFadeoutTime and CurrentTrack:currentTime() > 0.5)) then
-        NowPlayingOpacityTarget = 1
-    else
-        NowPlayingOpacityTarget = 0
-    end
-
-    if CurrentTrack and NowPlayingOpacityTarget == 1 and (CurrentTrack:currentTime() > CurrentTrack:duration()-2 or CurrentTrack:currentTime() < 1 or (SessionSwitched or TrackSwitched)) then
-        NowPlayingOpacityTarget = 0
-    end
-
-    if NowPlayingOpacityTarget < 0.75 and ui.mouseLocalPos().x > 0 and ui.mouseLocalPos().y > 0 and ui.mouseLocalPos().x < windowWidth.x+windowWidthRange+75*NowPlayingWidgetSize and ui.mouseLocalPos().y < 100*NowPlayingWidgetSize then
-        NowPlayingOpacityTarget = 0.75
-    end
-
-    if NowPlayingOpacityTarget > NowPlayingOpacityCurrent then
-        NowPlayingOpacityCurrent = NowPlayingOpacityCurrent + 0.02
-    elseif NowPlayingOpacityTarget < NowPlayingOpacityCurrent then
-        NowPlayingOpacityCurrent = NowPlayingOpacityCurrent - 0.02
-    end
-
-    if NowPlayingOpacityCurrent < 0.03 and NowPlayingOpacityTarget < 0.03 then
-        NowPlayingOpacityCurrent = 0
-    end
-
+if io.fileExists(ac.getFolder(ac.FolderID.ACApps) .. "/lua/DynamicMusicPlayer/NowPlaying Widgets/" .. NowPlayingWidgetDir .. "/Widget.lua") then
+    require('NowPlaying Widgets/' .. NowPlayingWidgetDir .. '/Widget')
+else
+    require('NowPlaying Widgets/Default/Widget')
 end
-
-require('NowPlayingV2')
-
-
 
 -- Keybindings
 
